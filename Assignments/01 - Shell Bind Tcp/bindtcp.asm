88e4850c65530ae2fedd485d121bf9600b0d595e
Reuben Sammut <reubensammut@gmail.com>
2014-03-19 07:20:48 +0100

    basic layout of bind tcp

---
+++ Assignments/01 - Shell Bind Tcp/bindtcp.asm
@@ -0,0 +1,36 @@
+; Filename: bindtcp.asm
+; Author:   Reuben Sammut
+; SLAE ID:  510
+
+global _start
+
+section .text
+
+_start:
+
+	; create socket
+
+
+
+	; bind
+
+
+	; listen
+
+
+	; accept
+	; fork
+
+	
+	;; child
+	;; dup2
+
+
+	;; execve
+
+
+
+	;; parent
+	;; waitpid
+	;; go back to accept
+
